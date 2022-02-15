6c1e464350680026984ed53e7733ba02e4c46abf
Venkatesan Rethinam <venkateeian@gmail.com>
2022-02-15 12:15:21 +0530

    Changes

---
+++ SchoolApp/Pages/Gallery.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolApp.Pages
+{
+    public class GalleryModel : PageModel
+    {
+        private readonly ILogger<GalleryModel> _logger;
+
+        public GalleryModel(ILogger<GalleryModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
